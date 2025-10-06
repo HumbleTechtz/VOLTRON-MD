208d1b707da60ddd3b364c18fd003335bc9cb297
HumbleTechtz <humbletechnology01@gmail.com>
2025-10-06 21:02:01 +0300

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+export default {
+  apps: [
+    {
+      name: 'voltron-md-multisession',
+      script: './index.js',
+      instances: 1,
+      exec_mode: 'fork',
+      watch: false,
+      max_memory_restart: '500M',
+      env: {
+        NODE_ENV: 'production',
+        NODE_OPTIONS: '--max-old-space-size=1024'
+      },
+      error_file: './logs/err.log',
+      out_file: './logs/out.log',
+      log_file: './logs/combined.log',
+      time: true,
+      restart_delay: 4000,
+      max_restarts: 10,
+      min_uptime: '10s'
+    }
+  ]
+};
